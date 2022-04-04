5d4add3b986bcf3e43ecbfacd31fe340161154af
geovannybolang <hezkiebolang@gmail.com>
2022-04-04 13:27:38 +0800

    Update Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,28 +5,25 @@ import { Button, Gap, Header, TextInput } from '../../components';
 const Home = () => {
   return (
     <View style={styles.page}>
-      <Header title="Money Tracker" />
-      <View style={styles.contentWrapper}>
-            <Text style={
-              {
-                  paddingLeft: 24,
-                  paddingVertical: 13,
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-              }
-          }> Your Balance</Text>
-          <Gap height={4} />
-            <Text style={
-              {
-                  textAlign: 'center',
-                  fontSize: 24,
-                  fontFamily: 'Poppins-Medium',
-                  
-
-              }
-          }>
-              Rp. 10.000.000</Text>
-        <View syle={styles.line} />
+        <View style={styles.profileWrapper}>
+          <Text style={styles.title}>
+            Money Tracker
+          </Text>
+          <Text style={styles.subTitle}>
+            Track Your Money
+          </Text>
+        <Gap height={24} />
+        <View style={styles.balanceWrapper}>
+          <Text style={styles.cardTitle}>
+            Your Balance
+          </Text>
+          <Text style={styles.totalBalance}>
+            Rp 10.000.000
+          </Text>
+          <Text>
+          _______________________________
+          </Text>
+        </View>
       </View>
     </View>
   )
@@ -39,19 +36,49 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 
-    contentWrapper:{
-        flex:1,
-        backgroundColor: "white",
-        marginTop: 24,
-        paddingHorizontal: 24,
+    profileWrapper:{
+      flex: 1,
+      paddingLeft: 24,
+      paddingVertical: 30,
+      backgroundColor: 'white',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
       },
 
-    line: {
-        width: 311,
-        height: 0,
-        border: 1,
+      title: {
+        paddingLeft: 20,
+        fontSize: 22,
+        fontFamily: 'Poppins-SemiBold',
+      },
 
-        backgroundColor: 'yellow',
-    },
+      subTitle: {
+        paddingLeft: 20,
+        fontSize: 14,
+        fontFamily: 'Poppins-Medium',
+      },
+
+      balanceWrapper: {
+        flex: 1,
+        marginTop: 24,
+        backgroundColor: 'white',
+        paddingHorizontal: 24,
+        paddingTop: 26,
+
+        
+      },
+
+      cardTitle: {
+        fontSize: 16,
+        fontFamily: 'Poppins-SemiBold',
+        textAlign: 'center',
+      },
+
+      totalBalance: {
+        fontSize: 24,
+        fontFamily: 'Poppins-SemiBold',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
 
 })
